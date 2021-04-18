b886c2ae4a9a7fd3ee638a77c7e25a7c6da2d4c2
Anton Dymov <dymov.ant@gmail.com>
2021-04-18 16:48:10 +0500

    Сверстал searchbar

--- src/components/searchbar/index.tsx
+++ src/components/searchbar/index.tsx
@@ -1,9 +1,14 @@
 import React, {FC} from "react"
+import searchIcon from "./searchIcon.svg"
 
 const Searchbar: FC = () => {
   return (
-    <div>
-      Searchbar
+    <div className="searchbar list-page__searchbar">
+      <img className="searchbar__img" src={searchIcon} alt=""/>
+      <input
+        className="searchbar__input"
+        type="text"
+      placeholder="Поиск пользователя GIthub"/>
     </div>
   )
 }
